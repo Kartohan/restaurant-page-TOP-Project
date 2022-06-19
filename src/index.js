77ee1ae78bbe20dc9436ab95d1c5dbe0c88d6b3d
Kartohan <Kartohan@gmail.com>
2022-06-19 21:06:01 +0300

    Changing about to menu

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import main from './home.js';
-import about from './about.js';
+import menuPage from './menu.js';
 import './style.css';
 
 let home = new main();
-let ab = new about();
+let ab = new menuPage();
 
 home.init();
 home.createUl('Home');
-home.createUl('About');
+home.createUl('Menu');
 home.createUl('Contact Us');
 home.createArticle('Try new!', "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias in fugiat exercitationem blanditi is at. Voluptatum sequi velit dolorem in exceptur i! Aliquid voluptates error molestiae consectetur pariatur corporis dolorum, id architecto quas di gnissimos voluptatum beatae velit delectus aperi am accusamus adipisci nisi aut doloremque nostru m laborum sapiente voluptas repudiandae qui quia ? Accusantium.")
 home.createArticle('We are open now!', "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laudantium laboriosam exercitationem error quasi, impedit aperiam unde vel eaque excepturi, porro omnis, optio atque voluptatibus odit ad hic deleniti consectetur consequuntur velit corporis sunt provident.")
 home.createArticle('Come here, we are waiting for you!', "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero ea deserunt, laboriosam eius eum magnam commodi maxime assumenda nemo enim architecto veritatis ut fugiat quam!");
 
 const ul = document.querySelector('ul');
+const btnMore = document.querySelector('.btn-more');
 function clear() {
     const li = document.querySelectorAll('li');
     const header = document.querySelector('.header');
@@ -23,7 +24,7 @@ function clear() {
     li.forEach(li => li.classList = '');
 }
 function menu(e) {
-    if (e.target.textContent === 'About') {
+    if (e.target.textContent === 'Menu') {
         if (e.target.className === '') {
             clear();
             ab.init();
@@ -37,7 +38,24 @@ function menu(e) {
             home.createArticle('Try new!', "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias in fugiat exercitationem blanditi is at. Voluptatum sequi velit dolorem in exceptur i! Aliquid voluptates error molestiae consectetur pariatur corporis dolorum, id architecto quas di gnissimos voluptatum beatae velit delectus aperi am accusamus adipisci nisi aut doloremque nostru m laborum sapiente voluptas repudiandae qui quia ? Accusantium.")
             home.createArticle('We are open now!', "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laudantium laboriosam exercitationem error quasi, impedit aperiam unde vel eaque excepturi, porro omnis, optio atque voluptatibus odit ad hic deleniti consectetur consequuntur velit corporis sunt provident.")
             home.createArticle('Come here, we are waiting for you!', "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero ea deserunt, laboriosam eius eum magnam commodi maxime assumenda nemo enim architecto veritatis ut fugiat quam!");
+            const btnMore = document.querySelector('.btn-more');
+            btnMore.addEventListener('click', function() {
+                let li = document.querySelectorAll('li');
+                let liMenu = [];
+                li.forEach(li => li.textContent === 'Menu' ? liMenu.push(li) : '');
+                clear();
+                ab.init();
+                liMenu[0].classList.add('Active');
+            })
         }
     }
 }
 ul.addEventListener('click', menu);
+btnMore.addEventListener('click', function() {
+    let li = document.querySelectorAll('li');
+    let liMenu = [];
+    li.forEach(li => li.textContent === 'Menu' ? liMenu.push(li) : '');
+    clear();
+    ab.init();
+    liMenu[0].classList.add('Active');
+})
---
+++ src/menu.js
@@ -0,0 +1,26 @@
+export default class menuPage {
+    init() {
+      const header = document.querySelector('.header');
+      const headerContent = document.createElement('div');
+      headerContent.classList.add('header-content');
+      const h1Header = document.createElement('h1');
+      h1Header.classList.add('about-header')
+      h1Header.textContent = 'Menu';
+      headerContent.appendChild(h1Header);
+      header.appendChild(headerContent);
+      const mainContent = document.querySelector('.main-content')
+
+      const menuContent = document.createElement('div');
+      menuContent.classList.add('menu-content');
+      const menuArticle = document.createElement('div');
+      menuArticle.classList.add('menu-article');
+      const h2Menu = document.createElement('h2');
+      const paraMenu = document.createElement('p');
+      
+
+
+      menuContent.appendChild(menuArticle);
+      mainContent.appendChild(menuContent);
+      }
+
+}
